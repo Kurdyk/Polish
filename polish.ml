8caaa81b10051ae0cc0e11f3002d0218f3e1e9ef
Victor Josso <victor@josso.fr>
2021-11-19 12:43:35 +0100

    --eval fait

--- polish.ml
+++ polish.ml
@@ -1,4 +1,4 @@
-open Format
+open Printf
 (** Projet Polish -- Analyse statique d'un mini-langage impératif *)
 
 (** Note : cet embryon de projet est pour l'instant en un seul fichier
@@ -65,15 +65,15 @@ let is_numeric str =
   with Failure _ -> false
 ;;
 
-(*
-let var_exists name = ENV.mem name !env;;
 
-let get_variable name = if ENV.mem name !env then ENV.find name !env else begin printf "Variable %s referenced before assignement" name; exit 1; end;;
+let var_exists name env = ENV.mem name env;;
 
-let check_variable_name name = if List.mem name exclusion_names then begin printf "Cannot name variable %s: forbidden name (reserved keyword)" name; exit 1; end else if is_numeric (String.make 1 (String.get name 0)) then begin printf "Cannot name variable %s: invalid name" name; exit 1; end;;
+let get_variable name env l = if ENV.mem name env then ENV.find name env else begin printf "Exception: Line %d, variable %s referenced before assignement" l name; exit 1; end;;
 
-let set_variable name value = check_variable_name name; env := ENV.add name value !env;;
-*)
+let check_variable_name name l = if List.mem name exclusion_names then begin printf "Exception: Line %d, cannot name variable %s: forbidden name (reserved keyword)\n" l name; exit 1; end else if is_numeric (String.make 1 (String.get name 0)) then begin printf "Exception: Line %d, cannot name variable %s: invalid name\n" l name; exit 1; end;;
+
+let set_variable name value env l = check_variable_name name l; ENV.add name value env;;
+
 
 let rec read_file channel current = 
   try
@@ -241,7 +241,58 @@ let print_polish (p:program) : unit =
       | h::t -> print_instr (snd h) current_block; printf "\n" ; interne current_block t;
   in interne 0 p;;
 
-let eval_polish (p:program) : unit = failwith "TODO"
+let rec eval_expr expr env l = match expr with 
+  | Num(a) -> a
+  | Var(v) -> get_variable v env l
+  | Op(operator, expr1, expr2) -> match operator with 
+    | Add -> (eval_expr expr1 env l) + (eval_expr expr2 env l)
+    | Sub -> (eval_expr expr1 env l) - (eval_expr expr2 env l)
+    | Mul -> (eval_expr expr1 env l) * (eval_expr expr2 env l)
+    | Div -> let divider = (eval_expr expr2 env l) in if divider = 0 then begin printf "Exception: Line %d, division by 0\n" l; exit 1 end else (eval_expr expr1 env l) / divider
+    | Mod -> let divider = (eval_expr expr2 env l) in if divider = 0 then begin printf "Exception: Line %d, division by 0\n" l; exit 1 end else (eval_expr expr1 env l) mod divider
+;;
+
+let eval_condition (expr1, comparator, expr2) env l = match comparator with 
+  | Eq -> (eval_expr expr1 env l) = (eval_expr expr2 env l)
+  | Ne -> (eval_expr expr1 env l) <> (eval_expr expr2 env l)
+  | Lt -> (eval_expr expr1 env l) < (eval_expr expr2 env l)
+  | Le -> (eval_expr expr1 env l) <= (eval_expr expr2 env l)
+  | Gt -> (eval_expr expr1 env l) > (eval_expr expr2 env l)
+  | Ge -> (eval_expr expr1 env l) >= (eval_expr expr2 env l)
+
+
+
+let print_expression expr env l = 
+  match expr with 
+    | Num(n) -> printf "%d\n" n
+    | Var(v) -> printf "%d\n" (get_variable v env l)
+    | Op(_, _, _) -> printf "%d\n" (eval_expr expr env l)
+;;
+
+
+let read_variable n env l = 
+  printf "%s? " n; 
+  let saisie = read_int () in 
+    ENV.add n saisie env;;
+
+let masquer_retour retour = ();;
+
+
+
+let eval_polish (p:program) =
+  let rec process_while condition block env l = 
+    if eval_condition condition env l then begin let new_env = parcours_programme block env in process_while condition block new_env l end else env
+  and parcours_programme prog env = match prog with 
+    | [] -> env
+    | (l, Print(expr)) :: suite_prog -> print_expression expr env l; parcours_programme suite_prog env
+    | (l, Set(name, expr)) :: suite_prog -> let new_env = set_variable name (eval_expr expr env l) env l in 
+          parcours_programme suite_prog new_env
+    | (l, Read(n)) :: suite_prog -> let new_env = read_variable n env l in parcours_programme suite_prog new_env
+    | (l, If(cond, block1, block2)) :: suite_prog -> let new_env = if eval_condition cond env l then parcours_programme block1 env else parcours_programme block2 env in parcours_programme suite_prog new_env
+    | (l, While(cond, block)) :: suite_prog -> let new_env = process_while cond block env l in parcours_programme suite_prog new_env
+  in masquer_retour (parcours_programme p ENV.empty)
+;;
+
 
 
 let usage () =
